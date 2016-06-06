d557988a9175a523cf78c5876f1b62ba51187ae8
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-06-06 16:32:08 +0100

    [OI] Add support for Centos 7. Configure influxdb.conf, startup and logrotate configs

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,6 +4,15 @@ describe package('influxdb') do
   it { should be_installed }
 end
 
+%w(
+  /etc/influxdb/influxdb.conf
+  /etc/logrotate.d/influxdb
+  /etc/default/influxdb
+).each do |f|
+  describe file(f) do
+  end
+end
+
 describe service('influxdb') do
   it { should be_running }
 end
